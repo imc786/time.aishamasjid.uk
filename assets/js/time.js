ec88c593ca1bdc1cc1e6431627759c3f21d3dc84
imc786 <90568340+imc786@users.noreply.github.com>
2023-08-18 12:27:33 +0100

    Video looping background

--- assets/js/time.js
+++ assets/js/time.js
@@ -292,7 +292,7 @@ async function timeCalc() {
   }
 }
 
-let images = ['assets/images/01.jpg', 'assets/images/02.jpg', 'assets/images/03.jpg', 'assets/images/04.jpg', 'assets/images/05.jpg', 'assets/images/06.jpg', 'assets/images/07.jpg', 'assets/images/08.jpg', 'assets/images/09.jpg', 'assets/images/10.jpg'];
+let images = ['assets/images/01.png', 'assets/images/02.png', 'assets/images/03.png', 'assets/images/04.png', 'assets/images/05.png', 'assets/images/06.png', 'assets/images/07.png', 'assets/images/08.png', 'assets/images/09.png', 'assets/images/10.png'];
 
 let index = 0;
 const imgElement = document.querySelector('#mainPhoto');
